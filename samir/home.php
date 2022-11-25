85742f49af504af226c84d47ff339dc94ed2dbaf
SonoMong <103244005+SonoMong@users.noreply.github.com>
2022-11-25 10:18:07 +0100

    Add files via upload

---
+++ samir/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" type="text/css" href="navbar.css" /> 
+  <title>TPI project</title>
+</head>
+<body>
+
+  <div class='navbar'>
+    
+    <a href='home.php'>Home</a>
+    <a href='elencoOG.php'>Oggetti smarriti</a>
+    <a href='contatti.php'>Contatti</a>
+    <a class='right' href='./Login/login.php'>Login</a>
+    
+  </div>
+<img class="backgroundmap" src="grey.jpg" alt="test">
+</body>
+</html>
