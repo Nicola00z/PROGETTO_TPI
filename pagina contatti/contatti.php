794bbf638f13be7a0de32b37e6330688ef59fdea
SonoMong <103244005+SonoMong@users.noreply.github.com>
2022-12-02 09:28:26 +0100

    Add files via upload
    pagina contatti finita

---
+++ pagina contatti/contatti.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" type="text/css" href="contatti_css.css" /> 
+  <link rel="stylesheet" type="text/css" href="navbar.css" /> 
+  <title>TPI project</title>
+</head>
+<body>
+
+  <div class='navbar'>
+    
+    <a href='home.php'>Home</a>
+    <a href='elencoOG.php'>Oggetti smarriti</a>
+    <a href='contatti.php'>Contatti</a>
+    <a class='right' href='./Login/login.php'>Login</a>
+    
+  </div>
+  <br>
+  <br>
+  <br>
+  <a href = 'https://www.marconiverona.edu.it/pagine/contattaci'>
+    <div class = "contatti" >
+    <img src="prova.png" alt="test">
+    <h1> Contattateci </h1>
+    <p>Dirigente Scolastico: Dott.ssa Gabriella Piccoli  Riceve su appuntamento – Contattare l’Ufficio Protocollo Direttore dei Servizi Generali... </p> 
+  </div></a>
+  <div class = "lista">
+    <p> <b>Dirigente Scolastico:</b> Dott.ssa Gabriella Piccoli. Riceve su appuntamento – Contattare l’Ufficio Protocollo
+      <br><br>
+      <b>Direttore dei Servizi Generali e Amministrativi:</b> Dott.ssa Franca Audia. Riceve su appuntamento – Contattare l’Ufficio Protocollo
+      <br><br>
+      <b>Responsabile Protezione dei Dati (Data Protection Officer):</b> Dott. Massimo Zampetti 
+      <br><br>
+      <b>RSPP (Responsabile del Servizio Prevenzione e Protezione):</b>  Prof. Giorgio Sileo
+      <br><br>
+        Informazioni: info@marconiverona.edu.it 
+      <br><br>
+       <b>Orario Segreteria Didattica</b>
+      <br><br>
+      Lunedì-Venerdi: dalle ore 7.40 alle ore 8.30 e dalle ore 11.30 alle ore 13.15
+      <br><br>
+      <b>Orario Protocollo e altri Uffici</b>
+      <br><br>
+      Lunedi-Venerdi: dalle ore 11.30 alle ore 13.15
+      <br><br>
+      <b>Il sabato l'Istituto rimane chiuso</b>
+      <br>
+      <br>
+      <b>Dove Siamo</b>
+      <br>
+      P.le R. Guardini 1, 37138 Verona
+      <br>
+        Tel. 045/8101428  - cod. Minist. VRTF03000V
+      <br>
+      E-mail: vrtf03000v@istruzione.it  - Pec: vrtf03000v@pec.istruzione.it                                                    
+      <br>
+      Cod. Fiscale 80020540235 
+      
+    </p>
+    <h2> Dove ci puoi trovare </h2>
+  </div>
+  <div class = "maps">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2800.0662171815616!2d10.9674239!3d45.4281665!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477f5f61f8852227%3A0xfab7331910c8e071!2sITIS%20Guglielmo%20Marconi!5e0!3m2!1sit!2sit!4v1669366702441!5m2!1sit!2sit" width="1067" height="700" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
+  <p>
+    
+  <div class ="container">
+    <p> P.le R. Guardini 1, 37138 Verona. &emsp; Tel: 045/8101428
+        <br>
+        Email: vrtf03000v@istruzione.it &emsp; &ensp; C.F.: 80020540235
+    </p>
+    </br>
+    </br>
+    </br>
+    </br>
+    
+  </div>
+
+</body>
